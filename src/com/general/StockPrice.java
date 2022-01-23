3f9ce144fb5db77fd4501f249a7b11497785ec62
zhouyu <2390851414@qq.com>
2022-01-23 17:12:10 +0800

    第2034题目完成

---
+++ src/com/general/StockPrice.java
@@ -0,0 +1,46 @@
+package com.general;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StockPrice {
+
+    private Map<Integer, Integer> map;
+    private TreeMap<Integer, Integer> treeMap;
+    private Integer curTime;
+
+    public StockPrice() {
+        this.curTime = Integer.MIN_VALUE;
+        this.map = new HashMap<>();
+        this.treeMap = new TreeMap<>();
+    }
+    public void update(int timestamp, int price) {
+        this.curTime = Math.max(this.curTime, timestamp);
+        if (this.map.containsKey(timestamp)) {
+            Integer oldPrice = this.map.get(timestamp);
+            int count = this.treeMap.get(oldPrice) - 1;
+            if (count == 0) {
+                this.treeMap.remove(oldPrice);
+            } else {
+                this.treeMap.put(oldPrice, count);
+            }
+
+        }
+        this.map.put(timestamp, price);
+        this.treeMap.put(price, this.treeMap.getOrDefault(price, 0) + 1);
+    }
+
+    public int current() {
+        return map.get(this.curTime);
+    }
+
+    public int maximum() {
+        return treeMap.lastKey();
+    }
+
+    public int minimum() {
+        return treeMap.firstKey();
+    }
+
+}
---
+++ src/com/general/Text2034.java
@@ -0,0 +1,21 @@
+package com.general;
+
+/**
+ * 解题思路：
+ *  使用Map保存数据：时间和价格；
+ */
+public class Text2034 {
+
+    public static void main(String[] args) {
+        StockPrice stockPrice = new StockPrice();
+        stockPrice.update(1, 10); // 时间戳为 [1] ，对应的股票价格为 [10] 。
+        stockPrice.update(2, 5);  // 时间戳为 [1,2] ，对应的股票价格为 [10,5] 。
+        System.out.println("stockPrice.current() = " + stockPrice.current());
+        System.out.println("stockPrice.maximum() = " + stockPrice.maximum());
+        stockPrice.update(1, 3);  // 之前时间戳为 1 的价格错误，价格更新为 3 。
+        // 时间戳为 [1,2] ，对应股票价格为 [3,5] 。
+        System.out.println("stockPrice.maximum() = " + stockPrice.maximum());
+        stockPrice.update(4, 2);  // 时间戳为 [1,2,4] ，对应价格为 [3,5,2] 。
+        System.out.println("stockPrice.minimum() = " + stockPrice.minimum());
+    }
+}
