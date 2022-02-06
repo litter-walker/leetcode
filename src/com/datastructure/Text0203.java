e0152ae03af64ffe13ee1fa67ff0046d6a6456a0
zhouyu <2390851414@qq.com>
2022-02-06 11:47:24 +0800

    2022/2/6, 数据结构-链表: 移除链表元素

---
+++ src/com/datastructure/Text0203.java
@@ -0,0 +1,28 @@
+package com.datastructure;
+
+import com.general.ListNode;
+
+/**
+ * 遍历：
+ *  使用新的头的next指向整个链表
+ */
+public class Text0203 {
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode newHead = new ListNode();
+        ListNode cur = newHead;
+        newHead.next = head;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return newHead.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
