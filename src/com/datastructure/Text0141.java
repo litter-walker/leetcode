6df620504162edc4963d7b792aecd790c72c87de
zhouyu <2390851414@qq.com>
2022-02-06 11:06:52 +0800

    2022/2/6, 数据结构-链表: 环形链表

---
+++ src/com/datastructure/Text0141.java
@@ -0,0 +1,30 @@
+package com.datastructure;
+
+import com.general.ListNode;
+
+/**
+ * 快慢双指针
+ */
+public class Text0141 {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
