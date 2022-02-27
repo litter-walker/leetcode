fb445e1f18541d712e69f660398dd4d93ad1c92c
zhouyu <2390851414@qq.com>
2022-02-27 21:33:01 +0800

    2022/2/27, 算法-二分查找: 二分查找

---
+++ src/com/algorithm/Text0704.java
@@ -0,0 +1,22 @@
+package com.algorithm;
+
+/**
+ * 二分查找
+ */
+public class Text0704 {
+
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
