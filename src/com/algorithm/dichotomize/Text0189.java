6ad36757caa547160e3b9aa05764cbdf7cca8f2d
zhouyu <2390851414@qq.com>
2022-02-28 23:20:25 +0800

    2022/2/28, 数据结构-数组: 轮转数组(数组复制)

---
+++ src/com/algorithm/dichotomize/Text0189.java
@@ -0,0 +1,19 @@
+package com.algorithm.dichotomize;
+
+/**
+ * 直接数组复制
+ */
+public class Text0189 {
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        if (k == 0 || nums.length < 2) {
+            return;
+        }
+        int[] intK = new int[k];
+        System.arraycopy(nums, nums.length - k, intK, 0, k);
+        System.arraycopy(nums, 0, nums, k, nums.length - k);
+        System.arraycopy(intK, 0, nums, 0, k);
+    }
+
+}
