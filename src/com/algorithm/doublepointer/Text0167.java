ccaa8a62c3e157a3a12fc5344d2c61babc7e0e53
zhouyu <2390851414@qq.com>
2022-03-01 19:14:46 +0800

    2022/3/1, 算法-双指针: 两数之和 II - 输入有序数组

---
+++ src/com/algorithm/doublepointer/Text0167.java
@@ -0,0 +1,22 @@
+package com.algorithm.doublepointer;
+
+/**
+ * 双指针
+ */
+public class Text0167 {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int l = 0, r = numbers.length - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) {
+                return new int[]{l + 1, r + 1};
+            } else if (sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
