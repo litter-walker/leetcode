103d2957b0ffb36a12dd4c51b85c32fffcedb6bf
zhouyu <2390851414@qq.com>
2022-03-01 22:48:54 +0800

    2022/3/1, 数据结构-数组: 旋转图像(模拟)

---
+++ src/com/datastructure/Text0048.java
@@ -0,0 +1,21 @@
+package com.datastructure;
+
+/**
+ * 模拟：
+ *  每次旋转4个元素；
+ */
+public class Text0048 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                int first = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = first;
+            }
+        }
+    }
+
+}
