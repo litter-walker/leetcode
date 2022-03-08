7a3f4a7608508b38b3d9b03ab81db7a0150f76c4
zhouyu <2390851414@qq.com>
2022-03-08 20:05:08 +0800

    2022/3/8, 算法-递归: 反转链表

---
+++ src/com/algorithm/recurseorbacktrack/Text0206.java
@@ -0,0 +1,22 @@
+package com.algorithm.recurseorbacktrack;
+
+import com.util.ListNode;
+
+/**
+ * 递归
+ */
+public class Text0206 {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
