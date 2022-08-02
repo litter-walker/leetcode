0ba80f51f2397bfc722296b659e21393be99ffd6
zhouyu <2390851414@qq.com>
2022-08-02 22:12:15 +0800

    2022/8/1, 每日一题-简单模拟: 生成每种字符都是奇数个的字符串

---
+++ src/com/general/text1374/Solution.java
@@ -0,0 +1,19 @@
+package com.general.text1374;
+
+/*
+    简单模拟
+ */
+public class Solution {
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+        if (n % 2 == 1) {
+            for (int i = 0; i < n; i++) {
+                sb.append('a');
+            }
+        } else {
+            sb.append('b');
+            for (int i = 1; i < n; i++) {
+                sb.append('a');
+            }
+        }
+        return sb.toString();
